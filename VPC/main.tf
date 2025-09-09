47592fbd317e8cafb02d68e88ad0152cf72a6bc7
Akash Upadhyaya <akashu0609@gmail.com>
2025-09-10 00:48:13 +0530

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.12.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
