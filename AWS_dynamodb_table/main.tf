92a21e8739e90fe02131d9538b4744412fd1e0a0
Akash Upadhyaya <akashu0609@gmail.com>
2025-09-10 00:51:39 +0530

    Update main.tf

--- AWS_dynamodb_table/main.tf
+++ AWS_dynamodb_table/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.12.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "GameScores"
   billing_mode   = "PROVISIONED"
