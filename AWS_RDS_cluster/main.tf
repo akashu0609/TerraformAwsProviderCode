e8ccfcc86a596abd885a2c55a178bb626a2ecead
Akash Upadhyaya <akashu0609@gmail.com>
2025-09-10 00:49:50 +0530

    Update main.tf

--- AWS_RDS_cluster/main.tf
+++ AWS_RDS_cluster/main.tf
@@ -1,8 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.12.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
 resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.03.2"
-  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  availability_zones      = ["us-east-1a", "us-east-1b", "us-east-1c"]
   database_name           = "mydb"
   master_username         = "foo"
   master_password         = "must_be_eight_characters"
